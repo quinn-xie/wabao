b35ae7a946e27e30c670a11a6c54b1aa75e639e7
Quinn.xie <80331554@qq.com>
2019-04-19 22:14:42 +0800

    登录接口添加

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
+import apiList from "utils/http.js"
+const api = require("utils/api.js") 
 App({
 
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   * 小程序登录
    */
   onLaunch: function () {
+    wx.login({
+      success: function (res) {
+        var code = res.code;
+        if (code) {
+          console.log('获取用户登录凭证：' + code);
+          // --------- 发送凭证 ------------------
+          api.post(apiList.member.wxlogin, code).then(res => {
 
+
+            console.log(res.timestamp)
+            const _token = JSON.stringify("023kRtrZ1OMnvS0uFtqZ1wGNrZ1kRtr6")
+            //const _token = JSON.stringify(res.token)
+            wx.setStorageSync('tokenId', _token)
+
+            
+          })
+        } else {
+          console.log('获取用户登录态失败：' + res.errMsg);
+        }
+      }
+    })
   },
-
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
